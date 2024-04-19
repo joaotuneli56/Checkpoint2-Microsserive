ef414164d998b0f9e7cf54c1e399f8c4ddf45eb9
Logon Aluno <logonrmlocal@fiap.com.br>
2024-04-18 21:47:59 -0300

    Checkpoint 2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { TarefaComponent } from './components/tarefas/tarefas.component';
+import { ClienteComponent } from './components/cliente/cliente.component';
+import { Tarefa } from './interfaces/tarefas';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, ClienteComponent,TarefaComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
